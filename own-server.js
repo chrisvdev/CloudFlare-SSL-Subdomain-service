c7dc00342d0dbce494c8885762571580eb0e7ca4
Christian Villegas <chrisvill.2312@gmail.com>
2022-12-12 01:07:00 +0000

    Added HTTPS support

--- own-server.js
+++ own-server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const PORT = parseInt(process.env.PORT);
+
 const express = require('express');
-
 const server = express();
+server.use("/", express.static("../web/"));
 
-server.use("/", express.static("../chrisvill2312.github.io/build"));
-server.listen(PORT, () => { console.log(`Server listening on port ${PORT}`) })
+const fs = require("fs");
+const cert = fs.readFileSync("./ssl/certificate.crt", "utf8")
+const key = fs.readFileSync("./ssl/private.key", "utf8")
+const credentials = { cert: cert, key: key }
+
+const https = require("https");
+const sslServer = https.createServer(credentials, server)
+
+server.listen(PORT, () => { console.log(`HTTP Server listening on port ${PORT}`) })
+sslServer.listen(PORT + 1, () => { console.log(`HTTPS Server listening on port ${PORT + 1}`) })
