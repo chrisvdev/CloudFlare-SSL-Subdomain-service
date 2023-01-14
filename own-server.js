689bc4cbe979d790af5b606ea8e945ee16badc07
Christian Villegas <chrisvill.2312@gmail.com>
2023-01-14 14:44:32 -0300

    WIP: Work on node module, some refactors

--- own-server.js
+++ own-server.js
@@ -1,23 +1,20 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+// import https from 'https'
+import fs from 'fs'
+import Cloudflare from './cf-api/cloudflare.js'
 
-require('dotenv').config()
+dotenv.config()
 const { CFEMAIL, CFKEY, CFZONE } = process.env
-const PORT = parseInt(process.env.PORT)
-const fs = require("fs")
-const key = fs.readFileSync("./ssl/private.key", "utf8")
+// const PORT = parseInt(process.env.PORT)
+const key = fs.readFileSync('./ssl/private.key', 'utf8')
+const server = express()
+server.use('/', express.static('./web/'))
 
-const express = require('express');
-const server = express();
-server.use("/", express.static("../web/"));
-const https = require("https");
-
-const Cloudflare = require('./cf-api/cloudflare')
 const myCloudflare = new Cloudflare(CFEMAIL, CFKEY, CFZONE)
-const boot = async () => {
-    const credentials = { cert: await myCloudflare.getCertificate(), key: key }
-    const sslServer = https.createServer(credentials, server)
-    sslServer.listen(PORT + 1, () => { console.log(`HTTPS Server listening on port ${PORT + 1}`) })
-    console.log(await myCloudflare.getDNSRecords())
-}
-
-boot()
-
+const certificates = await myCloudflare.getCertificates()
+/* const credentials = { cert: certificates[0].certificate, key }
+const sslServer = https.createServer(credentials, server)
+sslServer.listen(PORT, () => { console.log(`HTTPS Server listening on port ${PORT}`) }) */
+console.log(certificates[0].certificate)
+console.log(await myCloudflare.getDNSRecords('A'))
